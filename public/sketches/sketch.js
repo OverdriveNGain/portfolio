5088e503790a2b0f7e0e78ce11c307fb37cef906
jeremy <jeremyamon@gmail.com>
2021-07-22 21:45:22 +0800

    Optimized #sketch-floating-nodes and optimized its html structure

--- public/sketches/sketch.js
+++ public/sketches/sketch.js
@@ -1,12 +1,15 @@
 /* eslint no-undef: 0 */
 
-let pointManager = null;
+// FLOATING POINTS
+const md_bp = 768;
+
+let pointM = null;
 let windowWidthRep = null;
 let windowHeightRep = null;
-
+let floatingPointsAreaHeight = null;
 class PointManager {
   constructor() {
-    this.pointCount = round(windowWidth * windowHeight * 0.000217);
+    this.pointCount = round(width * height * 0.00008);
     this.points = [];
     this.regionCount = 4;
     this.regions = [];
@@ -15,13 +18,21 @@ class PointManager {
     }
   }
 
+  frame() {
+    stroke(0);
+    pointM.step();
+    pointM.draw();
+    noStroke();
+  }
+
   init() {
     const velMax = 1;
     const xUnit = (windowWidth / this.regionCount);
     const yUnit = (windowHeight / this.regionCount);
     this.points = [];
     for (let i = 0; i < this.pointCount; i++) {
-      let x = random(0, windowWidth);
+      // let x = random(0, windowWidth);
+      let x = random(0, windowWidth / 2);
       let y = random(0, windowHeight);
       this.points.push(new Point(x, y, random(-velMax, velMax), random(-velMax, velMax)));
       this.regions[floor(y / yUnit) * this.regionCount + floor(x / xUnit)] = i;
@@ -29,18 +40,18 @@ class PointManager {
   }
 
   draw() {
-    if (windowWidth > windowHeight) {
+    if (windowWidth > md_bp) {
       for (let i = 0; i < this.pointCount; i++) {
-        stroke(255 + -30 * ((abs(this.points[i].posx * 2 - windowWidth) * windowHeightRep)));
-        this.points[i].draw(i);
+        stroke(255 + -35 * ((abs(this.points[i].posx * 2 - windowWidth) * windowHeightRep)));
+        // this.points[i].draw(i);
         strokeWeight(1);
         this.points[i].drawConnections(i, this.points);
       };
     }
     else {
       for (let i = 0; i < this.pointCount; i++) {
-        stroke(255 + -30 * ((abs(this.points[i].posy * 2 - windowHeight) * windowWidthRep)));
-        this.points[i].draw(i);
+        stroke(255 + -35 * ((abs(this.points[i].posy * 2 - windowHeight) * windowWidthRep)));
+        // this.points[i].draw(i);
         strokeWeight(1);
         this.points[i].drawConnections(i, this.points);
       };
@@ -53,7 +64,6 @@ class PointManager {
     };
   }
 }
-
 class Point {
   constructor(posx, posy, velx, vely) {
     this.posx = posx;
@@ -71,52 +81,189 @@ class Point {
 
   drawConnections(thisI, points) {
     for (let i = thisI + 1; i < points.length; i++) {
-      if (abs(this.posx - points[i].posx) < 100 && abs(this.posy - points[i].posy) < 100)
-        // if (Point.distSquared(this.posx, this.posy, points[i].posx, points[i].posy) < (10000))
+      if (abs(this.posx - points[i].posx) < 100 && abs(this.posy - points[i].posy) < 100) {
         line(this.posx, this.posy, points[i].posx, points[i].posy);
+        line(width - this.posx, this.posy, width - points[i].posx, points[i].posy);
+      }
     }
   }
 
   draw(thisI) {
     strokeWeight(noise(thisI, frameCount * 0.01) * 4);
     point(this.posx, this.posy);
+    point(width - this.posx, this.posy);
   }
 
   step() {
     this.posx += this.velx;
     this.posy += this.vely;
     const margin = 50;
-    if (this.posx < -margin)
-      this.posx += windowWidth + margin * 2;
-    else if (this.posx > windowWidth + margin)
-      this.posx -= windowWidth - margin * 2;
+    if (width > md_bp) {
+      if (this.posx < -margin)
+        this.posx += width * 0.5 + margin;
+      else if (this.posx > width * 0.5)
+        this.posx -= width * 0.5 - margin;
+    }
+    else {
+      if (this.posx < -margin)
+        this.posx += width + margin * 2;
+      else if (this.posx > width + margin)
+        this.posx -= width - margin * 2;
+    }
+
     if (this.posy < -margin)
-      this.posy += windowHeight + margin * 2;
-    else if (this.posy > windowHeight + margin)
-      this.posy -= windowHeight - margin * 2;
+      this.posy += floatingPointsAreaHeight + margin * 2;
+    else if (this.posy > floatingPointsAreaHeight + margin)
+      this.posy -= floatingPointsAreaHeight + margin * 2;
+  }
+}
+
+// BORDER
+let col1 = [199, 0, 0];
+let col2 = [255, 255, 255];
+let borderM = null;
+let borderHeight = null;
+let randomSplashOnFrame = null;
+class ParticleManager {
+
+  frame() {
+    fill(col1);
+    rectMode(CORNERS);
+    rect(0, height - (borderHeight * 0.5), width, height);
+    const pbelow = pmouseY > height - borderHeight * 0.5;
+    const below = mouseY > height - borderHeight * 0.5;
+    if (pbelow && !below) {
+      borderM.splash(col1);
+      // particleM.splash(color(255, 0, 255));
+    }
+    else if (!pbelow && below) {
+      borderM.splash(col2);
+      // particleM.splash(color(0, 255, 255));
+    }
+    borderM.draw();
+    borderM.step();
+  }
+
+  constructor() {
+    this.particles = [];
+  }
+
+  splash(col) {
+    let velydif = mouseY - pmouseY;
+    let speed = min(dist(mouseX, mouseY, pmouseX, pmouseY) * 0.1, 10);
+    let angle = atan2(mouseY - pmouseY, mouseX - pmouseX)
+    const particleCount = min(60, floor(speed * 15));
+    for (let i = 0; i < particleCount; i++) {
+      let randomizedAngle = angle + (PI * randomGaussian() * 0.2);
+      let velMod = abs(randomGaussian(0, 1));
+      let velx = cos(randomizedAngle) * velMod * speed;
+      let vely = sin(randomizedAngle) * velMod * min(speed, borderHeight * 0.02);
+      if (vely > 0) {
+        vely *= 0.4;
+      }
+      let rad = i * 1.5;
+      let posy = height - borderHeight * 0.5 + (velydif > 0 ? -rad : rad) * 0.5;
+      let particle = new Particle(pmouseX + randomGaussian() * 10, posy, velx, vely, col, rad);
+      this.particles.push(particle);
+    }
+  }
+
+  draw() {
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      this.particles[i].draw();
+    }
+  }
+
+  step() {
+    for (let i = 0; i < this.particles.length; i++) {
+      this.particles[i].step();
+      if (this.particles[i].rad < 0.8) {
+        this.particles.splice(i, 1);
+        i--;
+      }
+    }
+  }
+}
+class Particle {
+  constructor(posx, posy, velx, vely, col, rad) {
+    this.startposy = posy;
+    this.startvely = vely;
+    this.posx = posx;
+    this.posy = posy;
+    this.velx = velx;
+    this.vely = vely;
+    this.accx = -velx * random(0.01, 0.04);
+    this.accy = -vely * random(0.01, 0.04);
+    this.col = col;
+    this.rad = rad;
+  }
+
+  draw() {
+    fill(this.col);
+    circle(this.posx, this.posy, this.rad)
+  }
+  step() {
+    this.posx += this.velx;
+    this.posy += this.vely;
+    this.velx += this.accx;
+    this.vely += this.accy;
+    this.rad -= 0.3;
+
+    if (this.startvely < 0) {
+      if (this.posy > this.startposy && this.vely > 0)
+        this.rad -= 0.8;
+    }
+    else {
+      if (this.posy < this.startposy && this.vely < 0)
+        this.rad -= 0.8;
+    }
   }
 }
 
 function setup() {
-  let sketch = createCanvas(windowWidth, windowHeight);
-  pointManager = new PointManager();
-  frameRate(30);
+  const e = document.getElementById("sketch-floating-nodes");
+  let sketch = createCanvas(round(e.offsetWidth), round(e.offsetHeight));
+  floatingPointsAreaHeight = round(e.offsetHeight) * 0.66666;
+  borderHeight = round(e.offsetHeight) * 0.66666;
 
-  pointManager.init();
+  pointM = new PointManager();
+
+  pointM.init();
   windowWidthRep = 1 / windowWidth;
   windowHeightRep = 1 / windowHeight;
+
+  borderM = new ParticleManager();
+  col1 = color(col1[0], col1[1], col1[2]);
+  col2 = color(col2[0], col2[1], col2[2]);
 
   sketch.parent('sketch-floating-nodes');
 }
+
+
 function draw() {
   background(255);
-  pointManager.step();
-  pointManager.draw();
+
+  pointM.frame()
+  borderM.frame();
+
+  fill(0);
+  text(round(frameRate()), 50, height - 300);
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  pointManager.init();
+  const e = document.getElementById("sketch-floating-nodes");
+  resizeCanvas(round(e.offsetWidth), round(e.offsetHeight));
+  floatingPointsAreaHeight = round(e.offsetHeight) * 0.66666;
+  borderHeight = round(e.offsetHeight) / 3;
+
+  pointM.init();
   windowWidthRep = 1 / windowWidth;
   windowHeightRep = 1 / windowHeight;
+
+  borderM = new ParticleManager();
+
+  col1 = color(col1[0], col1[1], col1[2]);
+  col2 = color(col2[0], col2[1], col2[2]);
+
+  sketch.parent('sketch-floating-nodes');
 }
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,22 +4,22 @@ const AboutMe = () => {
     useScript('sketches/sketch.js');
     return (
         <div id="about-me">
-            <div id="sketch-floating-nodes">
-
-            </div>
             <div id="about1">
-                <div className="">
-                    <div className="align-middle container py-3 text-center d-flex flex-column justify-content-center align-items-md-start">
-                        <div className="display-6 pt-5 pb-2 d-inline-block">So let me introduce myself...</div>
-                        <div className="display-2 pb-3 d-inline-block ps-md-2">My name is Jeremy,</div>
-                        <div className="display-6 pb-2 d-inline-block ps-md-5">and I am a full-stack software engineer.</div>
-                    </div>
+                <div id="sketch-floating-nodes" />
+                <div className="align-middle container py-3 text-center d-flex flex-column justify-content-center">
+                    <div className="display-6 pt-5 pb-2 d-inline-block">So let me introduce myself...</div>
+                    <div className="display-2 pb-3 d-inline-block">My name is Jeremy,</div>
+                    <div className="display-6 pb-2 d-inline-block">and I am a full-stack software engineer.</div>
                 </div>
             </div>
             <div id="about2">
-                <div className="bg-light">
-                    <div className="align-middle container py-3 text-center d-flex flex-column justify-content-center">
-                        <div className="lead d-inline-block px-5">I have a diverse set of skills that include mobile app and web app development, full-stack web development, and UI/UX design.</div>
+                <div className="">
+                    <div className="align-middle container py-3 text-center d-flex flex-column justify-content-center text-light">
+                        <div className="display-1">About Me</div>
+                        <div className="leading-3">I create mobile apps, websites, web apps, mobile games across virtually any platform. I highly believe that the mastery of a certain framework requires the mastery of all skills relevant to that framework. Thus, I take understanding of a certain project very seriously.</div>
+
+                        In any case, whether it be a website for your aspiring business, or a mobile app that unites your entire customer base,
+                        I get the job done, and more.
                     </div>
                 </div>
             </div>
