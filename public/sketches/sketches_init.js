2ffb646c08654e33eddb23dab3a5df0bda4aa533
jeremy <jeremyamon@gmail.com>
2021-07-23 22:47:40 +0800

    Extracted Particle Manager

--- public/sketches/sketches_init.js
+++ public/sketches/sketches_init.js
@@ -41,3 +41,102 @@ class Dust {
         this.vy = vy;
     }
 }
+
+class ParticleManager {
+    constructor(width, height, gaussianFunction) {
+        this.frame = 0;
+        this.particles = [];
+        this.randomSplashOnFrame = this.randomSplashInterval();
+        this.width = width;
+        this.height = height;
+        this.gaussian = gaussianFunction;
+    }
+    step() {
+        this.frame++;
+        if (this.frame === this.randomSplashOnFrame) {
+            this.splash();
+        }
+
+        for (let i = 0; i < this.particles.length; i++) {
+            let particle = this.particles[i];
+
+            particle.x += particle.vx;
+            particle.y += particle.vy;
+            particle.vx += particle.accx;
+            particle.vy += particle.accy;
+            particle.rad -= 0.3;
+
+            if (particle.startvy < 0) {
+                if (particle.y > particle.starty && particle.vy > 0)
+                    particle.rad -= 0.8;
+            }
+            else {
+                if (particle.y < particle.starty && particle.vy < 0)
+                    particle.rad -= 0.8;
+            }
+
+            if (particle.rad < 0.8) {
+                this.particles.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+    splash(mouseX, mouseY, pmouseX, pmouseY, colTop, colBot) {
+        let col;
+        if (mouseX === undefined) {
+            mouseX = Math.random() * this.width;
+            pmouseX = mouseX + (Math.random() * 5);
+            if (Math.random() > 0.5) {
+                mouseY = this.height / 2 - 12;
+                pmouseY = this.height / 2 + 12;
+                col = colTop;
+            }
+            else {
+                mouseY = this.height / 2 + 12;
+                pmouseY = this.height / 2 - 12;
+                col = colBot;
+            }
+        }
+
+        let velydif = mouseY - pmouseY;
+        let speed = Math.min(Math.hypot(mouseX - pmouseX, mouseY - pmouseY) * 0.1, 10);
+        let angle = Math.atan2(mouseY - pmouseY, mouseX - pmouseX)
+        const particleCount = Math.min(60, Math.floor(speed * 15));
+        for (let i = 0; i < particleCount; i++) {
+            let randomizedAngle = angle + (Math.PI * this.gaussian() * 0.2);
+            let velMod = Math.abs(this.gaussian());
+            let velx = Math.cos(randomizedAngle) * velMod * speed;
+            let vely = Math.sin(randomizedAngle) * velMod * Math.min(speed, this.height * 0.02);
+            if (vely > 0) {
+                vely *= 0.4;
+            }
+            let rad = i * 1.5;
+            let posy = (this.height + (velydif > 0 ? -rad : rad)) * 0.5;
+            let particle = new Particle(pmouseX + this.gaussian() * 10, posy, velx, vely, col, rad);
+            this.particles.push(particle);
+        }
+    }
+
+    randomSplashInterval() {
+        return 20;
+    }
+}
+class Particle {
+    constructor(x, y, vx, vy, col, rad) {
+        const r1 = Math.random() * 0.03 + 0.01;
+        const r2 = Math.random() * 0.03 + 0.01;
+        this.starty = y;
+        this.startvy = vy;
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        // this.accx = -vx * sketch.random(0.01, 0.04);
+        // this.accy = -vy * sketch.random(0.01, 0.04);
+        this.accx = -vx * r1;
+        this.accy = -vy * r2;
+        this.col = col;
+        this.rad = rad;
+    }
+}
