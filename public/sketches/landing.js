c6e3deb0cb1c0ea7763b96e46ce477c2ca54c6c0
jeremy <jeremyamon@gmail.com>
2021-07-23 21:55:46 +0800

    Finalized, extracted, and implemented Floating Point Manager

--- public/sketches/landing.js
+++ public/sketches/landing.js
@@ -1,47 +1,3 @@
-// import FloatingPointManager from "./sketches_init";
-
-class FloatingPointManager {
-  constructor(pointCount, width, height, maxvel) {
-    this.pointCount = pointCount;
-    this.points = [];
-    this.regionCount = 4;
-    this.width = width;
-    this.height = height;
-    this.points = [];
-    for (let i = 0; i < this.pointCount; i++) {
-      let x = Math.random() * this.width;
-      let y = Math.random() * this.height;
-      let nfp = new FloatingPoint(x, y, maxvel * 2 * Math.random() - maxvel, maxvel * 2 * Math.random() - maxvel);
-      this.points.push(nfp);
-    };
-  }
-  step() {
-    for (let i = 0; i < this.pointCount; i++) {
-      let point = this.points[i];
-
-      point.x += point.vx;
-      point.y += point.vy;
-
-      if (point.y < 0)
-        point.y += (this.height - 0.1);
-      else if (point.y > this.height)
-        point.y -= (this.height - 0.1);
-      if (point.x < 0)
-        point.x += (this.width - 0.1);
-      else if (point.x > this.width)
-        point.x -= (this.width - 0.1);
-    };
-  }
-}
-class FloatingPoint {
-  constructor(x, y, vx, vy) {
-    this.x = x;
-    this.y = y;
-    this.vx = vx;
-    this.vy = vy;
-  }
-}
-
 var landing1Function = (sketch) => {
 
   const floatingPointArea = 2 / 3;
@@ -219,6 +175,7 @@ var landing1Function = (sketch) => {
     sketch.borderHeight = sketch.round(e.offsetHeight) * 0.66666;
 
     // sketch.pointM = new PointManager(sketch);
+    // eslint-disable-next-line no-undef
     floatingPointM = new FloatingPointManager(50, sketch.width, Math.floor(sketch.height * floatingPointArea), 1);
     // sketch.pointM.init(sketch);
     sketch.borderM = new ParticleManager(sketch);
