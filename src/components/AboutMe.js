38cddc00e74533d010f9b913e53e2132a3b962bc
jeremy <jeremyamon@gmail.com>
2021-07-23 10:03:53 +0800

    Added basic Projects section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -21,11 +21,46 @@ const AboutMe = () => {
                 </div>
             </div>
             <div id="about3">
-                <div className="align-middle container py-3 text-center d-flex flex-column justify-content-center text-light bg-success">
-                    <div className="display-1">About Me</div>
-                    <div className="leading-3">I create mobile apps, websites, web apps, mobile games across virtually any platform. I highly believe that the mastery of a certain framework requires the mastery of all skills relevant to that framework. Thus, I take understanding of a certain project very seriously.</div>
-                    <p>In any case, whether it be a website for your aspiring business, or a mobile app that unites your entire customer base,</p>
-                    <p>I get the job done, and more.</p>
+                <div className="align-middle container py-3 text-center d-flex flex-column justify-content-center bg-success align-items-stretch border-0">
+                    <div className="display-1 text-light my-5">Projects</div>
+                    <div className="row mx-xs-2 mx-md-5">
+                        <div className="col-12 col-md-6 mb-4">
+                            <a href="/"><div className="card">
+                                <div className="card-body m-1 m-md-4">
+                                    <h5 className="card-title no-underline">Card title</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div></a>
+                        </div>
+                        <div className="col-12 col-md-6 mb-4">
+                            <a href="/"><div className="card">
+                                <div className="card-body m-1 m-md-4">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div></a>
+                        </div>
+                    </div>
+                    <div className="row mx-xs-2 mx-md-5">
+                        <div className="col-12 col-md-6 mb-4">
+                            <a href="/"><div className="card">
+                                <div className="card-body m-1 m-md-4">
+                                    <h5 className="card-title no-underline">Card title</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div></a>
+                        </div>
+                        <div className="d-none d-md-flex col-8 col-md-6 mb-4">
+                            <a href="/"><div className="card">
+                                <div className="card-body m-1 m-md-4">
+                                    <h5 className="card-title">Last Card title</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div></a>
+                        </div>
+                    </div>
+
+                    <button className="btn btn-primary align-self-center m-3">See More Projects</button>
                 </div>
             </div>
         </div>
