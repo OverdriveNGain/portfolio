d1bde96dc0de2a5a6b6b9b9bf1e66a8be3ff6627
jeremy <jeremyamon@gmail.com>
2021-07-25 22:00:28 +0800

    Added secondary color or dark orange

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -49,16 +49,16 @@ const AboutMe = () => {
                 <div id="landing1" />
                 <div className="align-middle container py-3 text-center d-flex flex-column justify-content-center">
                     <div className="display-6 pt-5 pb-2 d-inline-block text-muted">So let me introduce myself...</div>
-                    <div className="display-2 pb-3 d-inline-block">My name is Jeremy,</div>
-                    <div className="display-6 pb-2 d-inline-block">and I am a full-stack software engineer.</div>
+                    <div className="display-2 pb-3 d-inline-block">My name is <span className="text-primary">Jeremy</span>,</div>
+                    <div className="display-6 pb-2 d-inline-block ">and I am a <span className="text-secondary">full-stack software engineer</span>.</div>
                 </div>
             </div>
             <div id="about2">
                 <div className="align-middle container py-3 text-center d-flex flex-column justify-content-center text-light">
                     <div className="display-1 m-3 font-title">About Me</div>
-                    <div className="lead">I create mobile apps, websites, web apps, Unity games to be played on multiple platforms.</div>
+                    <div className="mb-3">I create mobile apps, websites, web apps, Unity games to be played on multiple platforms.</div>
                     <p> I highly believe that the mastery of a certain framework requires the mastery of all skills relevant to that framework. Thus, I take understanding of a certain project very seriously. In any case, whether it be a website for your aspiring business, or a mobile app that unites your entire customer base,</p>
-                    <p>I get the job done, and more.</p>
+                    <p className="lead fw-bold">I get the job done, and more.</p>
                 </div>
             </div>
             <div id="about3">
@@ -69,7 +69,7 @@ const AboutMe = () => {
                         <div className="col-12 col-md-6 mb-4">
                             <a href="/"><div className="card">
                                 <div className="card-body m-1 m-md-4">
-                                    <h5 className="card-title no-underline">Card title</h5>
+                                    <h5 className="card-title no-underline text-secondary">Card title</h5>
                                     <p className="card-text text-dark">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 </div>
                             </div></a>
@@ -77,7 +77,7 @@ const AboutMe = () => {
                         <div className="col-12 col-md-6 mb-4">
                             <a href="/"><div className="card">
                                 <div className="card-body m-1 m-md-4">
-                                    <h5 className="card-title">Card title</h5>
+                                    <h5 className="card-title text-secondary">Card title</h5>
                                     <p className="card-text text-dark">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 </div>
                             </div></a>
@@ -87,7 +87,7 @@ const AboutMe = () => {
                         <div className="col-12 col-md-6 mb-4">
                             <a href="/"><div className="card">
                                 <div className="card-body m-1 m-md-4">
-                                    <h5 className="card-title no-underline">Card title</h5>
+                                    <h5 className="card-title no-underline text-secondary">Card title</h5>
                                     <p className="card-text text-dark">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 </div>
                             </div></a>
@@ -95,7 +95,7 @@ const AboutMe = () => {
                         <div className="d-none d-md-flex col-8 col-md-6 mb-4">
                             <a href="/"><div className="card">
                                 <div className="card-body m-1 m-md-4">
-                                    <h5 className="card-title">Last Card title</h5>
+                                    <h5 className="card-title text-secondary">Last Card title</h5>
                                     <p className="card-text text-dark">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 </div>
                             </div></a>
@@ -114,8 +114,8 @@ const AboutMe = () => {
                             <a href="/"><div className={`card border-0 rounded rounded-on-md shadow`}
                                 onMouseOver={(e) => landing_services_hover(e, 0)} onMouseOut={(e) => landing_services_hover(e, -1)}>
                                 <div className="card-body m-1 m-md-4">
-                                    <i className="bi bi-brush display-1"></i>
-                                    <h5 className="card-title no-underline">Graphic Design</h5>
+                                    <i className="bi bi-brush display-1 text-secondary"></i>
+                                    <h5 className="card-title no-underline text-secondary">Graphic Design</h5>
                                     <p className="card-text text-dark">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                     <small className="text-muted">Adobe Illustrator • Adobe Photoshop • p5.js • Processing • Python</small>
                                 </div>
@@ -124,8 +124,8 @@ const AboutMe = () => {
                         <div className="col-12 col-md-4">
                             <a href="/"><div className="card border-0 rounded rounded-on-md shadow" onMouseOver={(e) => landing_services_hover(e, 1)} onMouseOut={(e) => landing_services_hover(e, -1)}>
                                 <div className="card-body m-1 m-md-4">
-                                    <i className="bi bi-globe2 display-1"></i>
-                                    <h5 className="card-title">Website Development</h5>
+                                    <i className="bi bi-globe2 display-1 text-secondary"></i>
+                                    <h5 className="card-title text-secondary">Website Development</h5>
                                     <p className="card-text text-dark">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                     <small className="text-muted">HTML • CSS • Javascript • SASS • React • Flutter • Firebase • Bootstrap 5 • git • GitHub</small>
                                 </div>
@@ -134,8 +134,8 @@ const AboutMe = () => {
                         <div className="col-12 col-md-4">
                             <a href="/"><div className="card border-0 rounded rounded-on-md shadow" onMouseOver={(e) => landing_services_hover(e, 2)} onMouseOut={(e) => landing_services_hover(e, -1)}>
                                 <div className="card-body m-1 m-md-4">
-                                    <i className="bi bi-phone display-1"></i>
-                                    <h5 className="card-title">Mobile App Development</h5>
+                                    <i className="bi bi-phone display-1 text-secondary"></i>
+                                    <h5 className="card-title text-secondary">Mobile App Development</h5>
                                     <p className="card-text text-dark">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                     <small className="text-muted">React • Flutter • Adobe XD • Firebase • Unity 3D • git • GitHub</small>
                                 </div>
@@ -146,12 +146,12 @@ const AboutMe = () => {
 
             </div>
             <div id="about5">
-                <div className="align-middle container px-0 py-3 text-center d-flex flex-column justify-content-center align-items-stretch">
+                <div className="align-middle container px-0 py-5 my-5 text-center d-flex flex-column justify-content-center align-items-stretch">
                     <div className="display-1 text-primary mb-5 font-title">Testimonials</div>
                     <div className="row justify-content-center align-items-center text-center">
                         <div className="col-7">
                             <blockquote>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quisquam magni adipisci neque vero! Odit ab, expedita repellat aliquam magnam, vitae sed totam ea sequi eaque, laborum accusantium minima officiis."</blockquote>
-                            <div className="rounded-circle border mx-auto border-primary border-2 my-4" id="testimonial-portrait-holder">
+                            <div className="rounded-circle border mx-auto border-secondary border-2 my-4" id="testimonial-portrait-holder">
                                 <img src="https://previews.123rf.com/images/gladkov/gladkov1312/gladkov131200051/24403725-closeup-portrait-of-young-man-call-center-employee-with-a-headset-isolated-on-white-background.jpg" alt="Testimonial Person Portrait" />
                             </div>
                             <div className="fw-bold">Firstname M. Last</div>
@@ -162,8 +162,8 @@ const AboutMe = () => {
             <div id="about6" className="bg-light">
                 <div className="align-middle container px-0 py-3 text-center d-flex flex-column 
                 justify-content-center align-items-stretch px-4 py-5">
-                    <div className="display-6 text-primary">Need to get something done?</div>
-                    <div className="display-3 text-primary mb-4 font-title">Let’s Talk!</div>
+                    <div className="display-6 text-muted">Need to get something done?</div>
+                    <div className="display-3 text-secondary mb-4 font-title">Let’s Talk!</div>
                     <div className="row justify-content-center">
                         <div className="col col-7 text-start">
                             <form onSubmit={(e) => { submitCallback(e) }}>
