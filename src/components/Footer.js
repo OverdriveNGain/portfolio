e43bf44b3bcb3cccac843edbd8eabe09387aff14
jeremy <jeremyamon@gmail.com>
2021-07-31 00:19:18 +0800

    Added footer quote

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ const Footer = () => {
     return (
         <footer>
             <div className="bg-dark p-5 text-center">
+                <p className="text-muted mb-4"><i>"Quality means doing it right when no-one one is looking."   — Henry Ford</i></p>
                 <a href="https://www.facebook.com/jeremy.m.amon/"><i className="px-3 h3 text-light bi bi-facebook"></i></a>
                 <a href="https://twitter.com/AmonDevs"><i className="px-3 h3 text-light bi bi-twitter"></i></a>
                 <a href="mailto: jeremyamon@gmail.com"><i className="px-3 h3 text-light bi bi-envelope-fill"></i></a>
