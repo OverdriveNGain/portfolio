921d2788b0fe1d26d64d9e93c11f35c406aa5f57
jeremy <jeremyamon@gmail.com>
2021-07-31 17:59:38 +0800

    Separated navbar brand opacity from navbar bg opacity

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,26 +5,30 @@ import {
 } from "react-router-dom";
 
 const Navbar = () => {
-    const [transparent, setTransparent] = useState(true);
-    const transparentThreshold = 300;
+    const [transparentBrand, setTransparentBrand] = useState(true);
+    const [transparentBg, setTransparentBg] = useState(true);
+    const transparentBrandThreshold = 300;
+    const transparentBgThreshold = 150;
 
     const location = useLocation();
 
     useEffect(() => {
-        setTransparent(document.documentElement.scrollTop <= transparentThreshold);
+        setTransparentBrand(document.documentElement.scrollTop <= transparentBrandThreshold);
+        setTransparentBg(document.documentElement.scrollTop <= transparentBgThreshold);
     }, []);
     useEffect(() => {
-        const onScroll = e => {
-            setTransparent(e.target.documentElement.scrollTop <= transparentThreshold);
+        const onScroll = () => {
+            setTransparentBrand(document.documentElement.scrollTop <= transparentBrandThreshold);
+            setTransparentBg(document.documentElement.scrollTop <= transparentBgThreshold);
         };
         window.addEventListener("scroll", onScroll);
 
         return () => {
             return window.removeEventListener("scroll", onScroll);
         };
-    }, [transparent]);
+    }, [transparentBrand, transparentBg]);
 
-    let transparentCheck = transparent && location.pathname === "/";
+    let transparentCheck = transparentBg && location.pathname === "/";
     const inlineNavbarStyle = {
         "backgroundColor": (transparentCheck ? "transparent" : "rgba(255, 255, 255, 0.92)"),
         "backdropFilter": (transparentCheck ? "none" : "blur(6px)")
@@ -36,7 +40,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-md navbar-light fixed-top animated-background-color" style={inlineNavbarStyle}>
             <div className="container px-4">
-                <Link className={"animated-opacity navbar-brand" + (transparent && location.pathname === "/" ? " opacity-0" : "")} to="/">
+                <Link className={"animated-opacity navbar-brand" + (transparentBrand && location.pathname === "/" ? " opacity-0" : "")} to="/">
                     <div className="fw-bold text-primary b-0">Jeremy Mattheu D. Amon</div>
                     <small className="text-muted">Full-Stack Software Engineer</small>
                 </Link>
