62c769f3f298b748b112cd4c6036eec150b530f9
jeremy <jeremyamon@gmail.com>
2021-08-02 01:41:43 +0800

    Rearranged landing2.js members

--- public/sketches/landing2.js
+++ public/sketches/landing2.js
@@ -3,49 +3,12 @@ var landing2FunctionSetVisible;
 
 var landing2Function = (sketch) => {
 
-    let shouldRemove = false;
-    landing2FunctionSetVisible = (remove) => {
-        if (remove === undefined) {
-            const element = document.getElementById('landing2');
-            let position = element.getBoundingClientRect();
-            const isVisible = (position.top < window.innerHeight && position.bottom >= 0);
-            if (isVisible)
-                sketch.loop();
-            else
-                sketch.noLoop();
-        }
-        else if (remove)
-            shouldRemove = true;
-    }
-
-    const drawBorderWaves = () => {
-        sketch.fill(col2);
-        sketch.rectMode(sketch.CORNERS);
-        sketch.rect(0, 0, sketch.width, sketch.height * 0.5);
-        for (let i = borderM.particles.length - 1; i >= 0; i--) {
-            // this.particles[i].draw(sketch);
-            let particle = borderM.particles[i];
-            sketch.fill(particle.col);
-            sketch.circle(particle.x, particle.y, particle.rad)
-        }
-    }
-
-    const borderWaveSplashCheck = () => {
-        const pbelow = sketch.pmouseY > sketch.height - sketch.height * 0.5;
-        const below = sketch.mouseY > sketch.height - sketch.height * 0.5;
-        if (pbelow && !below) {
-            borderM.splash(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY, 0.4, 0.4);
-        }
-        else if (!pbelow && below) {
-            borderM.splash(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY, 0.4, 0.4);
-        }
-    }
-
     let col2 = [78, 104, 255];
     let col1 = [255, 255, 255];
     let borderM;
-
+    let shouldRemove = false;
     let element;
+
     sketch.setup = () => {
         element = document.getElementById("landing2");
         if (element === null) {
@@ -112,6 +75,43 @@ var landing2Function = (sketch) => {
 
         sketch.fill(0);
         sketch.text(sketch.round(sketch.frameRate()), 50, sketch.height - 300);
+    }
+
+    landing2FunctionSetVisible = (remove) => {
+        if (remove === undefined) {
+            const element = document.getElementById('landing2');
+            let position = element.getBoundingClientRect();
+            const isVisible = (position.top < window.innerHeight && position.bottom >= 0);
+            if (isVisible)
+                sketch.loop();
+            else
+                sketch.noLoop();
+        }
+        else if (remove)
+            shouldRemove = true;
+    }
+
+    const drawBorderWaves = () => {
+        sketch.fill(col2);
+        sketch.rectMode(sketch.CORNERS);
+        sketch.rect(0, 0, sketch.width, sketch.height * 0.5);
+        for (let i = borderM.particles.length - 1; i >= 0; i--) {
+            // this.particles[i].draw(sketch);
+            let particle = borderM.particles[i];
+            sketch.fill(particle.col);
+            sketch.circle(particle.x, particle.y, particle.rad)
+        }
+    }
+
+    const borderWaveSplashCheck = () => {
+        const pbelow = sketch.pmouseY > sketch.height - sketch.height * 0.5;
+        const below = sketch.mouseY > sketch.height - sketch.height * 0.5;
+        if (pbelow && !below) {
+            borderM.splash(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY, 0.4, 0.4);
+        }
+        else if (!pbelow && below) {
+            borderM.splash(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY, 0.4, 0.4);
+        }
     }
 };
 // eslint-disable-next-line no-unused-vars
