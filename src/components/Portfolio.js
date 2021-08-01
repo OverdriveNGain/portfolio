59586158b0ca4ab9a48dba66257d42c2d46e9396
jeremy <jeremyamon@gmail.com>
2021-08-01 17:57:16 +0800

    Added specified colors for each programming language

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,7 +5,7 @@ const Portfolio = () => {
     useScript('sketches/portfolio_bg.js');
 
     useEffect(() => {
-        window.scrollTo(0, 0);
+        // window.scrollTo(0, 0);
         return (() => {
             try {
                 // eslint-disable-next-line no-undef
@@ -49,11 +49,49 @@ const Portfolio = () => {
             </div>
         );
     }
+    const getBadgeStyle = (b) => {
+        switch (b) {
+            case "Flutter":
+                return { backgroundColor: "rgb(0, 180, 232)", color: "rgb(255, 255, 255)" };
+            case "Unity 3D":
+                return { backgroundColor: "rgb(240, 240, 240)", color: "rgb(30, 30, 30)" };
+            case "Firebase":
+                return { backgroundColor: "rgb(255, 202, 41)", color: "rgb(90, 90, 90)" };
+            case "React JS":
+                return { backgroundColor: "rgb(32, 34, 36)", color: "rgb(96, 216, 249)" };
+            case "HTML":
+                return { backgroundColor: "rgb(228, 77, 38)", color: "white" };
+            case "CSS":
+                return { backgroundColor: "rgb(55, 154, 214)", color: "white" };
+            case "Javascript":
+                return { backgroundColor: "rgb(247, 223, 30)", color: "black" };
+            case "SASS":
+                return { backgroundColor: "rgb(205, 102, 154)", color: "white" };
+            case "Bootstrap":
+                return { backgroundColor: "rgb(140, 19, 253)", color: "white" };
+            case "C#":
+                return { backgroundColor: "rgb(104, 33, 122)", color: "white" };
+            case "Python":
+                return { backgroundColor: "rgb(54, 110, 156)", color: "rgb(255, 226, 130)" };
+            case "Adobe Illustrator":
+                return { backgroundColor: "rgb(51, 0, 0)", color: "rgb(255, 154, 0)" };
+            case "Processing":
+                return { backgroundColor: "rgb(1, 37, 66)", color: "white" };
+            case "p5.js":
+                return { backgroundColor: "rgb(234, 37, 96)", color: "white" };
+            case "Adobe Photoshop":
+                return { backgroundColor: "rgb(0,30,54)", color: "rgb(49,168,255)" };
+            case "Inkscape":
+                return { backgroundColor: "rgb(224, 224, 224)", color: "black" };
+            default:
+                return { backgroundColor: "black", color: "white" };
+        }
+    }
     const toSkillBadges = (s) => {
         const skillsArray = s.split(",").map((s) => s.trim());
         const badges = [];
         for (let i = 0; i < skillsArray.length; i++) {
-            badges.push(<span key={i} className="badge bg-muted me-1 mb-1 fw-normal">{skillsArray[i]}</span>);
+            badges.push(<span key={i} className="badge me-1 mb-1 fw-normal fw-bold" style={getBadgeStyle(skillsArray[i])}>{skillsArray[i]}</span>);
         }
         return (
             <div className="mb-3">
@@ -104,7 +142,7 @@ const Portfolio = () => {
                                 {toSkillsBullet("Experienced uploading to the Google Play Store with a Google Play developer account, while having uploaded multiple Unity 3D games")}
 
                                 {toSkillsHeader("Digital Artist and Illustrator")}
-                                {toSkillBadges("Adobe Illustrator, Processing, Adobe Photoshop")}
+                                {toSkillBadges("Adobe Illustrator, Adobe Photoshop, Processing, p5.js, Inkscape")}
                                 {toSkillsBullet("Creates digital designs to be made into products such as stickers, magnets, and vector art for Just Crafts PH, an online sticker shop, where 90% of all social media pubs, and 100% of all sticker designs are developed by him using Adobe Illustrator")}
                                 {toSkillsBullet("Sketches and constructs digital shirt designs and ID lace designs for Trinity University of Asia")}
                                 {toSkillsBullet("Created billboards and publications for the Colegio de Santo Cristo de Burgosschool, as well as designed the school's yearbooks from 2014 to 2020")}
