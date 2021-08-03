5e1d7d403d2ad44e9d9f13847839b49cd8836aa7
jeremy <jeremyamon@gmail.com>
2021-08-03 20:18:53 +0800

    Automatic navbar button close

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -47,28 +47,41 @@ const Navbar = () => {
     const getNavbarLinkColor = (locationObject, targetPath) => {
         return locationObject.pathname === targetPath ? "text-secondary" : "text-primary";
     }
+
+    const navbarLinkClick = () => {
+        console.log("Hello!");
+        document.getElementById('nav-toggle').click();
+    }
     return (
         <nav className="navbar navbar-expand-md navbar-light fixed-top animated-background-color" style={inlineNavbarStyle}>
             <div className="container px-4">
                 <NavbarBrand location={location} scrollTop={scrollTop} />
                 <button className="navbar-toggler" data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavAltMarkup">
+                    data-bs-target="#navbarNavAltMarkup" id="nav-toggle">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
                         {breakpointSelector(
-                            <Link className={`nav-link px-3 ${getNavbarLinkColor(location, "/aboutme")}`} to="/aboutme" style={expandBgNavSmall}>About Me</Link>, null, null,
-                            <Link className={`nav-link px-3 bg-md-success ${getNavbarLinkColor(location, "/aboutme")}`} to="/aboutme">About Me</Link>)}
+                            <Link className={`nav-link px-3 ${getNavbarLinkColor(location, "/aboutme")}`}
+                                onClick={navbarLinkClick} to="/aboutme" style={expandBgNavSmall}>About Me</Link>, null, null,
+                            <Link className={`nav-link px-3 bg-md-success ${getNavbarLinkColor(location, "/aboutme")}`}
+                                to="/aboutme">About Me</Link>)}
                         {breakpointSelector(
-                            <Link className={`nav-link px-3 ${getNavbarLinkColor(location, "/projects")}`} to="/projects" style={expandBgNavSmall}>Projects</Link>, null, null,
-                            <Link className={`nav-link px-3 bg-md-success ${getNavbarLinkColor(location, "/projects")}`} to="/projects">Projects</Link>)}
+                            <Link className={`nav-link px-3 ${getNavbarLinkColor(location, "/projects")}`}
+                                onClick={navbarLinkClick} to="/projects" style={expandBgNavSmall}>Projects</Link>, null, null,
+                            <Link className={`nav-link px-3 bg-md-success ${getNavbarLinkColor(location, "/projects")}`}
+                                to="/projects">Projects</Link>)}
                         {breakpointSelector(
-                            <Link className={`nav-link px-3 ${getNavbarLinkColor(location, "/portfolio")}`} to="/portfolio" style={expandBgNavSmall}>Portfolio</Link>, null, null,
-                            <Link className={`nav-link px-3 bg-md-success ${getNavbarLinkColor(location, "/portfolio")}`} to="/portfolio">Portfolio</Link>)}
+                            <Link className={`nav-link px-3 ${getNavbarLinkColor(location, "/portfolio")}`}
+                                onClick={navbarLinkClick} to="/portfolio" style={expandBgNavSmall}>Portfolio</Link>, null, null,
+                            <Link className={`nav-link px-3 bg-md-success ${getNavbarLinkColor(location, "/portfolio")}`}
+                                to="/portfolio">Portfolio</Link>)}
                         {breakpointSelector(
-                            <Link className={`nav-link px-3 ${getNavbarLinkColor(location, "/contact")}`} to="/contact" style={expandBgNavSmall}>Contact</Link>, null, null,
-                            <Link className={`nav-link px-3 bg-md-success ${getNavbarLinkColor(location, "/contact")}`} to="/contact">Contact</Link>)}
+                            <Link className={`nav-link px-3 ${getNavbarLinkColor(location, "/contact")}`}
+                                onClick={navbarLinkClick} to="/contact" style={expandBgNavSmall}>Contact</Link>, null, null,
+                            <Link className={`nav-link px-3 bg-md-success ${getNavbarLinkColor(location, "/contact")}`}
+                                to="/contact">Contact</Link>)}
 
                     </div>
                 </div>
