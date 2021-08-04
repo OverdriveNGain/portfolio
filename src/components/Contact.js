95664fe65a9b62b91ad151fe93db9a09ecce3d32
jeremy <jeremyamon@gmail.com>
2021-08-04 13:27:59 +0800

    Tweaked with Contact page appearance

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,51 +8,52 @@ const Contact = () => {
         window.scrollTo(0, 0);
     }, []);
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col col-md-10 px-4">
-                    <p className="py-5" />
-                    <p className="display-1 font-title text-primary text-center">Contact Me</p>
-                    <div className="my-5">
-                        <div className="lead text-center">I would love to hear from you!</div>
-                        <div className="row text-center bg-light p-4 m-4">
-                            <div className="col mb-3">
-                                <div className="text-secondary fw-bold">Email</div>
-                                <div className="display-6">jeremyamon@gmail.com</div>
-                            </div>
-                            <div className="col mb-3">
-                                <div className="text-secondary fw-bold">LinkedIn</div>
-                                <div className="display-6"><a href="">Jeremy Amon</a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="lead text-center">You can also reach me directly through this form:</div>
-                    <div className="align-middle container px-0 py-3 text-center d-flex flex-column 
-                justify-content-center align-items-stretch px-4 pt-4 pb-5">
-                        <div className="row justify-content-center">
-                            <div className="col col-7 text-start">
-                                <div className="p-5 shadow rounded-3 bg-light">
+        <div>
+            <div className="container">
+                <div className="px-5">
+                    <div className="row">
+                        <div className="col-8 pe-5 ">
+                            <p className="py-5" />
+                            <p className="display-1 font-title text-primary">Contact Me</p>
+                            <div className="lead text-primary pb-4">I would love to hear from you!</div>
+                            <div className="align-middle text-center d-flex flex-column justify-content-center align-items-stretch pb-5">
+                                <div className="">
                                     <form onSubmit={(e) => { submitCallback(e) }}>
                                         <div className="mb-3">
-                                            <label htmlFor="contactName" className="form-label fw-bold text-secondary">Name</label>
-                                            <input type="name" className="form-control" id="contactName" placeholder="John Doe" name="fname" />
+                                            <input type="name" className="form-control" id="contactName" placeholder="Your Name" name="fname" />
                                         </div>
                                         <div className="mb-3">
-                                            <label htmlFor="contactEmail" className="form-label fw-bold text-secondary">Email</label>
-                                            <input type="email" className="form-control" id="contactEmail" placeholder="name@example.com" name="email" />
+                                            <input type="email" className="form-control" id="contactEmail" placeholder="Your Email" name="email" />
                                         </div>
                                         <div className="mb-3">
-                                            <label htmlFor="contactMessage" className="form-label fw-bold text-secondary">Message</label>
-                                            <textarea className="form-control" id="contactMessage" rows="10" name="message"></textarea>
+                                            <textarea className="form-control" id="contactMessage" placeholder="Type your message here..." rows="10" name="message"></textarea>
                                         </div>
                                         <div className="text-center"><button className="btn btn-primary text-light" type="submit"><i className="bi bi-envelope-fill pe-2"></i>Send</button></div>
                                     </form>
                                 </div>
                             </div>
                         </div>
+                        <div className="col-4 bg-light p-5">
+                            <p className="py-5" />
+                            <div class="pb-4">
+                                <i class="bi bi-envelope-fill pe-3"></i>
+                                <span className="">jeremyamon@gmail.com</span>
+                            </div>
+                            <div class="pb-4">
+                                <i class="bi bi-linkedin pe-3"></i>
+                                <span className=""><a href="https://www.linkedin.com/in/jeremy-amon-b0548a124/">Jeremy Amon on LinkedIn</a></span>
+                            </div>
+                            <div class="pb-4">
+                                <i class="bi bi-github pe-3"></i>
+                                <span className=""><a href="https://github.com/OverdriveNGain">Jeremy Amon on Github</a></span>
+                            </div>
+                            <div class="pb-4">
+                                <i class="bi bi-phone-fill pe-3"></i>
+                                <span className="">+63 995 963 2500</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
             </div>
         </div>
     );
