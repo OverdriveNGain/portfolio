3bf0ed33ca35154adee675b6f7158f80d9ea6658
jeremy <jeremyamon@gmail.com>
2021-08-04 11:16:48 +0800

    Added basic Contact Page info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,11 +13,25 @@ const Contact = () => {
                 <div className="col col-md-10 px-4">
                     <p className="py-5" />
                     <p className="display-1 font-title text-primary text-center">Contact Me</p>
-                    <div id="about6" className="bg-light">
-                        <div className="align-middle container px-0 py-3 text-center d-flex flex-column 
-                justify-content-center align-items-stretch px-4 py-5">
-                            <div className="row justify-content-center">
-                                <div className="col col-7 text-start">
+                    <div className="my-5">
+                        <div className="lead text-center">I would love to hear from you!</div>
+                        <div className="row text-center bg-light p-4 m-4">
+                            <div className="col mb-3">
+                                <div className="text-secondary fw-bold">Email</div>
+                                <div className="display-6">jeremyamon@gmail.com</div>
+                            </div>
+                            <div className="col mb-3">
+                                <div className="text-secondary fw-bold">LinkedIn</div>
+                                <div className="display-6"><a href="">Jeremy Amon</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="lead text-center">You can also reach me directly through this form:</div>
+                    <div className="align-middle container px-0 py-3 text-center d-flex flex-column 
+                justify-content-center align-items-stretch px-4 pt-4 pb-5">
+                        <div className="row justify-content-center">
+                            <div className="col col-7 text-start">
+                                <div className="p-5 shadow rounded-3 bg-light">
                                     <form onSubmit={(e) => { submitCallback(e) }}>
                                         <div className="mb-3">
                                             <label htmlFor="contactName" className="form-label fw-bold text-secondary">Name</label>
@@ -29,7 +43,7 @@ const Contact = () => {
                                         </div>
                                         <div className="mb-3">
                                             <label htmlFor="contactMessage" className="form-label fw-bold text-secondary">Message</label>
-                                            <textarea className="form-control" id="contactMessage" rows="3" name="message"></textarea>
+                                            <textarea className="form-control" id="contactMessage" rows="10" name="message"></textarea>
                                         </div>
                                         <div className="text-center"><button className="btn btn-primary text-light" type="submit"><i className="bi bi-envelope-fill pe-2"></i>Send</button></div>
                                     </form>
@@ -37,10 +51,9 @@ const Contact = () => {
                             </div>
                         </div>
                     </div>
-
                 </div>
-            </div>
 
+            </div>
         </div>
     );
 }
