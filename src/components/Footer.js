fd3ec20a0607cbb909d7bcc5dc2ad1c2c95c4403
jeremy <jeremyamon@gmail.com>
2021-08-04 20:38:33 +0800

    Fixed short page footer bug (hack)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,20 @@
 const Footer = () => {
+    /*Hack to make footer at the bottom of page*/
     return (
         <footer>
-            <div className="bg-dark p-5 text-center">
+            <div className="bg-dark p-5 text-center" style={{
+                opacity: "0%"
+            }}>
+                <p className="text-white mb-4"><i>"Quality means doing it right when no-one one is looking."   — Henry Ford</i></p>
+                <div className="container text-muted text-center mt-4">
+                    © 2021 jeremyamon.com - All Rights Reserved.
+                </div>
+            </div>
+            <div className="bg-dark p-5 text-center" style={{
+                bottom: "0px",
+                width: "100%",
+                position: "absolute",
+            }}>
                 <p className="text-white mb-4"><i>"Quality means doing it right when no-one one is looking."   — Henry Ford</i></p>
                 <div className="container text-muted text-center mt-4">
                     © 2021 jeremyamon.com - All Rights Reserved.
