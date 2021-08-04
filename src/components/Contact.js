c874faed9f71b989c4e1335db213831fce8e6567
jeremy <jeremyamon@gmail.com>
2021-08-04 14:26:56 +0800

    Made contact page responsize

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,70 @@
 import { useEffect } from "react";
+import useResize from '../hooks/useResize';
 
 const Contact = () => {
+    const { dimensions, breakpointSelector } = useResize();
     const submitCallback = (e) => {
 
     }
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
+    const getOtherInfoPanel = (part) => {
+        if (part === "right") {
+            return breakpointSelector(null, null, null,
+                (
+                    <div className="col-lg-4 bg-light p-5">
+                        <p className="d-lg-block p-5" />
+                        <div class="pb-4">
+                            <i class="bi bi-envelope-fill pe-3"></i>
+                            <span className="">jeremyamon@gmail.com</span>
+                        </div>
+                        <div class="pb-4">
+                            <i class="bi bi-linkedin pe-3"></i>
+                            <span className=""><a href="https://www.linkedin.com/in/jeremy-amon-b0548a124/">Jeremy Amon on LinkedIn</a></span>
+                        </div>
+                        <div class="pb-4">
+                            <i class="bi bi-github pe-3"></i>
+                            <span className=""><a href="https://github.com/OverdriveNGain">Jeremy Amon on Github</a></span>
+                        </div>
+                        <div class="pb-4">
+                            <i class="bi bi-phone-fill pe-3"></i>
+                            <span className="">+63 995 963 2500</span>
+                        </div>
+                    </div>));
+        }
+        if (part === "bot")
+            return breakpointSelector(
+                (
+                    <div className="bg-light">
+                        <div className="container p-4 text-center">
+                            <div className="row ps-4 ps-sm-0 justify-content-center">
+                                <div class="pb-4 col-12 col-sm-6">
+                                    <i class="bi bi-envelope-fill pe-3"></i>
+                                    <span className="">jeremyamon@gmail.com</span>
+                                </div>
+                                <div class="pb-4 col-12 col-sm-6">
+                                    <i class="bi bi-phone-fill pe-3"></i>
+                                    <span className="">+63 995 963 2500</span>
+                                </div>
+                                <div class="pb-4 col-12 col-sm-6">
+                                    <i class="bi bi-linkedin pe-3"></i>
+                                    <span className=""><a href="https://www.linkedin.com/in/jeremy-amon-b0548a124/">Jeremy Amon on LinkedIn</a></span>
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <i class="bi bi-github pe-3"></i>
+                                    <span className=""><a href="https://github.com/OverdriveNGain">Jeremy Amon on Github</a></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>), null, null, <div></div>);
+    }
     return (
         <div>
             <div className="container">
-                <div className="px-5">
+                <div className="px-lg-5">
                     <div className="row">
-                        <div className="col-8 pe-5 ">
+                        <div className="col-12 col-lg-8 px-4">
                             <p className="py-5" />
                             <p className="display-1 font-title text-primary">Contact Me</p>
                             <div className="lead text-primary pb-4">I would love to hear from you!</div>
@@ -33,28 +85,11 @@ const Contact = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-4 bg-light p-5">
-                            <p className="py-5" />
-                            <div class="pb-4">
-                                <i class="bi bi-envelope-fill pe-3"></i>
-                                <span className="">jeremyamon@gmail.com</span>
-                            </div>
-                            <div class="pb-4">
-                                <i class="bi bi-linkedin pe-3"></i>
-                                <span className=""><a href="https://www.linkedin.com/in/jeremy-amon-b0548a124/">Jeremy Amon on LinkedIn</a></span>
-                            </div>
-                            <div class="pb-4">
-                                <i class="bi bi-github pe-3"></i>
-                                <span className=""><a href="https://github.com/OverdriveNGain">Jeremy Amon on Github</a></span>
-                            </div>
-                            <div class="pb-4">
-                                <i class="bi bi-phone-fill pe-3"></i>
-                                <span className="">+63 995 963 2500</span>
-                            </div>
-                        </div>
+                        {getOtherInfoPanel("right")}
                     </div>
                 </div>
             </div>
+            {getOtherInfoPanel("bot")}
         </div>
     );
 }
