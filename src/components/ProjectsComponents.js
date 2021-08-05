616a0ed8c21595a3d6f69e6a822357b2c4ba0562
jeremy <jeremyamon@gmail.com>
2021-08-05 12:39:24 +0800

    Added github link

--- src/components/ProjectsComponents.js
+++ src/components/ProjectsComponents.js
@@ -15,6 +15,20 @@ const ProjectDetailsPage = ({ projectData }) => {
         proj = response;
     if (proj == null)
         return (<div>Loading...</div>);
+
+    const getGitHubLink = () => {
+        if (proj.github != null) {
+            return (
+                <p>
+                    <a href="https://github.com/OverdriveNGain/portfolio" target="_blank" rel="noopener noreferrer">
+                        Open this project in Github
+                    </a>
+                </p>
+            );
+        }
+        return <div></div>;
+    }
+
     return (
         <div>
             <div className="row">
@@ -24,6 +38,7 @@ const ProjectDetailsPage = ({ projectData }) => {
                 <div className="col">
                     <h2>{proj.title}</h2>
                     <p>{proj.descLong}</p>
+                    {getGitHubLink()}
                     <div>This project uses the following frameworks and tools:</div>
                     <p className="text-muted fw-bold">{proj.languages.join(", ")}</p>
                 </div>
