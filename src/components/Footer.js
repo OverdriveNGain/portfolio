e4587d81ceca8b5f17b1a55f64e068164cb290ba
jeremy <jeremyamon@gmail.com>
2021-08-07 22:55:34 +0800

    Made spacing footer all white

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,11 @@ const Footer = () => {
     /*Hack to make footer at the bottom of page*/
     return (
         <footer>
-            <div className="bg-dark p-5 text-center" style={{
+            <div className="bg-white p-5 text-center" style={{
                 opacity: "0%"
             }}>
                 <p className="text-white mb-4"><i>"Quality means doing it right when no-one one is looking."   — Henry Ford</i></p>
-                <div className="container text-muted text-center mt-4">
+                <div className="container text-white text-center mt-4">
                     © 2021 jeremyamon.com - All Rights Reserved.
                 </div>
             </div>
