97368ca43e98673e504200e1e16ae3cbefced33c
jeremy <jeremyamon@gmail.com>
2021-08-08 10:37:26 +0800

    Added fix for no delay useScript

--- src/hooks/useScript.js
+++ src/hooks/useScript.js
@@ -3,11 +3,14 @@ import { useEffect } from 'react';
 const useScript = (url, after) => {
     useEffect(() => {
         const script = document.createElement('script');
+        let time = 0;
+        if (after != null)
+            time = after;
         setTimeout(() => {
             script.src = url;
             script.async = true;
             document.body.appendChild(script);
-        }, after);
+        }, time);
         return () => {
             document.body.removeChild(script)
         }
