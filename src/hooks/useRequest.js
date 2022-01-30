9bdb411db3ad00ffb445be861d182e862955c426
Jeremy Amon <jdamon@up.edu.ph>
2022-01-30 19:54:06 +0800

    Removed debug-db stuff

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -1,12 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
-import { fromJsonDB } from "./db_debug";
 
-//options
-// {
-//     enable: true,
-//     alt: "http://localhost:3004"
-// }
 const useRequest = (uri, options = {}) => {
     const [response, setResponse] = useState(null);
 
@@ -17,17 +11,8 @@ const useRequest = (uri, options = {}) => {
             if (options.enable === true)
                 return;
             let uriToUse = uri;
-            if (fromJsonDB) {
-                if (options.alt != null)
-                    uriToUse = options.alt;
-                else
-                    console.error(`useRequest: jsonDB flat is set to true, but an alternate URI is not provided for the following URI: ${uri}`);
-            }
             axios.get(uriToUse).then((res) => {
-                if (fromJsonDB)
-                    setResponse(res.data);
-                else
-                    setResponse(res.data.data);
+                setResponse(res.data.data);
             });
         }
         getDataFromServer();
