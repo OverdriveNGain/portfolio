d76ed09aeab6bdb8ec7f133cdc25167e6a54d914
Jeremy Mattheu D. Amon <jeremy.amon@paymaya.com>
2024-08-24 17:22:53 +0800

    Updated About Me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,25 +13,26 @@ const AboutMe = () => {
                     <p className="display-1 font-title text-primary text-center">About Me</p>
                     <p className="mt-4 display-6">Greetings!</p>
                     <p className="mb-3">
-                        My name is Jeremy, and I am a full-stack developer. I create websites, mobile apps, and software that help in a multitude of tasks, for everyone to use. I enjoy programming and logic problems, so I happily enjoy my time whenever I create various projects in my free time.
+                        My name is Jeremy, and I am a software engineer.
                     </p>
                     <p className="mb-3">
-                        In essence, I create software with a focus on accuracy and performance. I provide well-documented commits to my repositories, and I dive deep into whatever framework I dive into. I believe that the quality of a system is only as good as the quality of its subparts, and following that belief system in engineering accessible software and applications will lead to better quality outputs.
+                        I create software with a focus on accuracy, usability, and maintainability. As a programmer, you should never leave a codebase in a confusing state. That means meaningful variable names, including comments within code, documentation, refactoring code whenever needed, and even updating the project README file. If you are the programmer and you are having a difficult time understanding the code, then that is an indication of poor code quality that needs to be addressed if project maintainability is important.
                     </p>
                     <p className="mb-3">
-                        My passion for programming sparked when I first wanted to create my own computer game. When I was 11, I used Game Maker Studio 1 to try and create simple games using sprites made in Inkscape. Although I wasn't able to finish any projects due to school homework, I am thankful for the experience because I have learned a lot of programming concepts from studying Game Maker’s programming language, GML.
+                        I also believe in writing tickets for every note about the application that needs to be addressed. This does not only include bugs, but also minimal user interface issues, UX considerations, and code quality concerns. Having a well-documented list of tasks that you need to do for the application allows for a more streamlined development process that helps build software confidence within the team.
                     </p>
                     <p className="mb-3">
-                        A few years later, I taught myself how to use another freely available game engine, Unity 3D. This time, I was able to finish a few games using that platform. Although they were simple games, I did all of the work myself, including sprite creation using Adobe Illustrator, SFX using Audacity, and uploading APKs to the Google Play Store. Of course, my games didn’t go viral, but I am especially thankful for the skills that I have learned because of Unity as well, as I was able to use these skills in other aspects of my life (which included creating pub mats for school projects, creating billboards for a school, and designing T-shirts and ID laces every semester)
+                        My passion for programming sparked when I first wanted to create my own computer game. When I was 11, I used Game Maker Studio to try and create simple games using sprites made in Inkscape. This was the springboard for me to transition from drag-and-drop programming to writing code, and I am thankful for the core memory because it is where the journey started for me.
                     </p>
                     <p className="mb-3">
-                        Fast forward to today, I have grown a lot more in terms of the frameworks and tools that I use for not only games but also for websites and mobile applications. I can create a website for your business that focuses on user experience and ease of use, or I can develop your mobile app that provides convenience to your customers to which they truly deserve. I work well in teams, and I learn fast in any environment that you assign to me.
+                        I studied Computer Science at the University of the Philippines. They have taught me topics like Computer Architecture, Operating Systems, Computer Networks, Compiler Design, Databases, and a whole lot of math. I can confidently say that they have prepared me well for the corporate world, and the knowledge that I have acquired is not to be forgotten.
                     </p>
-                    <Link to="/contact">
-                        <p className="">
-                            Let’s grab a cup of coffee sometime!
-                        </p>
-                    </Link>
+                    <p className="mb-3">
+                        Fast forward to today, I am currently working at Maya Philippines, happily working a 9 to 6. I have learned a lot of software development practices in the projects that I have been assigned to so far, and my enthusiasm for it has been overwhelming that I have also started implementing these practices in my own personal projects.
+                    </p>
+                    <p className="mb-3">
+                        If you want to know more about me, feel free to <Link to="/contact">send me a message</Link>!
+                    </p>
                     <p className="py-5" />
                 </div>
             </div>
